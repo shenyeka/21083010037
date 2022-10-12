a41d3ce60252be354a6a7c4aaed066175623196a
ShenyEkaOktaviani <113080415+ShenyEkaOktaviani@users.noreply.github.com>
2022-10-12 16:38:27 +0700

    Add files via upload

---
+++ Array_CA.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#deklarasi array compound assignment
+distroLinuxDesktop=('BlankOn' 'Ubuntu' 'Debian' 'ArchLinux' 'LinuxMint')
+distroLinuxServer=('UbuntuServer' 'CentOS' 'FedoraServer')
+
+#cara mengambil nilai array
+echo ${distroLinuxDesktop[*]}
+echo ${distroLinuxServer[*]}
